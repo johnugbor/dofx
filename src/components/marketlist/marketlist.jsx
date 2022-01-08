ae19b7c3ae1ffbc05254f61ce28a42f6f5c8ce8a
John Ugbor <ugborjohn@gmail.com>
2022-01-08 16:50:10 +0100

    marketlist.jsx now works with the current slice file

--- src/components/marketlist/marketlist.jsx
+++ src/components/marketlist/marketlist.jsx
@@ -17,6 +17,7 @@ function MarketList(props) {
 	var LeftToggle = props.LeftToggleProps;
 	const [favouriteStar, setFavouriteStar] = useState(false);
 	const [searchExist, setSearchExist] = useState(false);
+												 
 	const authTokens = useSelector(state=>state.access.access).token
 	const dispatch = useDispatch()
 	const toggleStar = () => {
@@ -32,6 +33,7 @@ function MarketList(props) {
 				(resp)=>{
 					if(resp.status===200){
 						console.log(resp.data);
+					  
 						dispatch(updateAsset(resp.data));
 					}
 				}
@@ -147,7 +149,7 @@ function MarketList(props) {
 											</button>
 											: <button class="asset-open" onClick={() => {
 												setModalShow(true);
-												dispatch(edit(index.data));
+												dispatch(edit(index));
 											}
 
 											}>
@@ -162,7 +164,7 @@ function MarketList(props) {
 														Add chart
 													</Tooltip>}>
 												<button type="button" class="asset-button asset-button__first" data-toggle="tooltip" data-placement="bottom" title="Add chart"
-													onClick={() => dispatch(edit(index.data))}>
+													onClick={() => dispatch(edit(index))}>
 													<svg viewBox="0 0 31.49 31.49">
 														<path d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111  C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587  c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z">
 														</path>
